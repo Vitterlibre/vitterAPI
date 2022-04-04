b798bad2ea69deff2eb34dc8dd11a5c6711569d3
unfathomable99 <guzmanr.pablo@outlook.es>
2022-04-03 22:26:27 -0500

    Creacion de ID y forma JSON para bounding boxes y trends

--- Vitter_API/api/twitter/trends.py
+++ Vitter_API/api/twitter/trends.py
@@ -1,22 +1,31 @@
+import random
+import string
+import tweepy
 import credentials
 import tweepy
-
+import time
+import sys
+import json
 
 auth = tweepy.OAuthHandler(credentials.API_KEY, credentials.API_SECRET_KEY)
 auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+
+# Create API object
 api = tweepy.API(auth)
+
+# WOEID of Colombia
 woeid = 23424787
   
 # fetching the trends
 trends = api.get_place_trends(id = woeid)
 tendencias = []
-# printing the information
-print("Los top 5 trend de Colombia son:")
-  
+alphabet = string.ascii_lowercase + string.digits
+   
 for value in trends:
     for trend in value['trends']:
-        tendencias.append(trend['name'])
-count = 0
-while count < 5:
-    print(tendencias[count])
-    count = count + 1    
+        tendencias.append({'id':''.join(random.choices(alphabet, k=8)), 'trend':trend['name']},)
+
+json_object = json.dumps(tendencias, indent= 4)
+
+
+
